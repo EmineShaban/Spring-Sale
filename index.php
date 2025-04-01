dbfbe469e053d6da4fe3cfac8c3f5521f317f323
Emine Shaban <emineshaban@icloud.com>
2025-04-01 08:19:33 +0300

    .

---
+++ index.php
@@ -0,0 +1,54 @@
+            <!-- STEP2 -->
+            <div class="spring-sale">
+                <img class="mobile" src="https://lesthe.com/cdn/onycostop/inc/assets-general/spring-sale/images/Step_2_Onyco_Spring-mobile-05.jpg" alt="">
+                <img class="desktop" src="https://lesthe.com/cdn/onycostop/inc/assets-general/spring-sale/images/Step_2_Onyco_Spring-desktop-03.jpg" alt="">
+                <div class="spring-sale-div">
+                    <img class="" src="https://lesthe.com/cdn/onycostop/inc/assets-general/spring-sale/images/Step_2_Onyco_Spring-04.webp" alt="">
+                    <div class="spring-sale-bottom-div">
+                        <div>
+                            <p class="desktop"><?php echo $LANG['up_to'] ?></p>
+                            <p>-60% <?php echo $LANG['off'] ?></p>
+                        </div>
+                        <div>
+                            <p><?php echo $LANG['with_code'] ?><span>SPRING10</span></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <!-- INDEX -->
+
+    <section class="spring-sale">
+        <img class="mobile" src="/cdn/onycostop/inc/assets-general/spring-sale/images/Index_Onyco_SPRING-mobile-03.jpg" alt="">
+        <img class="desktop" src="/cdn/onycostop/inc/assets-general/spring-sale/images/Index_Onyco_SPRING-desktop-05.jpg" alt="">
+
+        <div class="spring-sale-div">
+            <div class="spring-sale-top">
+            <?php echo $LANG['prepare_your_feet'] ?>
+            <?php echo $LANG['stay_fungi_free'] ?>
+                <a class="spring-sale-cta" href="<?php echo $LANG['link_to_order_page'] ?>"><?php echo $LANG['order_now'] ?></a>
+            </div>
+            <div class="spring-sale-bottom">
+                <img class="" src="/cdn/onycostop/inc/assets-general/spring-sale/images/Index_Onyco_SPRING-04.webp" alt="">
+                <div class="spring-sale-bottom-div">
+                    <div>
+                        <p><?php echo $LANG['up_to'] ?></p>
+                        <p>-60% <?php echo $LANG['off'] ?></p>
+                    </div>
+                    <div>
+                        <p><?php echo $LANG['with_code'] ?><span>SPRING10</span></p>
+                    </div>
+                </div>
+            </div>
+        </div> 
+    </section>
+    
+
+
+    $LANG['prepare_your_feet'] = "<p>prepare your feet for the spring</p>"; 
+    $LANG['stay_fungi_free'] = "<p>stay fungi-free!</p>"; 
+
+
+
+
